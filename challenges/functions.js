ff83c73194aaeae314080b619410c1219d3c5cb3
Roger Caballero <rogercp1234@yahoo.com>
2019-04-05 08:56:51 -0700

    functions page is completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,39 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(a,b,cb){
+console.log(cb(a,b));
+}
+
+
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add= function add(a,b){
+  return a+b
+}
+const multiply=function multiply(a,b){
+  return a*b;
+}
+const greeting=function greeting(first_name,last_name){
+  return `Hello ${first_name} ${last_name}, nice to meet you`;
+}
+
+
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+
+
+
 
 
 // ==== Closures ==== 
@@ -26,7 +47,9 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+//This is because of a concept knows as closures where a function has access to variables outside of this scope.
+//In this case the nestedFunction() is inside the scope of myFunctions but its enclosed in its own scope however it 
+//does have access to the saved value of that variable because of javascript two pass compilation.
 
 const external = "I'm outside the function";
 
